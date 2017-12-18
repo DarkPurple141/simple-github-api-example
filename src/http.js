4cf80eec7d74134601d436bcaecd3387b3e10104
Alex Hinds <alex.hinds141@gmail.com>
2017-12-18 14:49:31 +1100

    deleted old GPG key

--- src/http.js
+++ src/http.js
@@ -1,12 +1,9 @@
 
 import API from 'simple-github-api'
 
-// GPG key 5aefdc0252c0eac731b92bd78e9e42bb1bd4ec5e
+// const GPG_KEY = ""
 
-const HTTP = new API(
-   {
-      token: "5aefdc0252c0eac731b92bd78e9e42bb1bd4ec5e"
-   })
+const HTTP = new API()
 
 
 export default HTTP;
